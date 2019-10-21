e8f8b7f827556c892321609ad6a675f5ae325b16
Danish <danish@3gca.org>
2019-10-21 22:14:15 +0500

    Project Created

---
+++ README.md
@@ -0,0 +1,36 @@
+# reverse-search README
+
+This is the README for your extension "reverse-search".
+Reverse Search functionality is to search files that do not contain specific string in them. In visual studio code we can search
+for files where specific string exists but there are no such options for Reverse Search.
+We can use this extension for multiple purposes. i.e checking which files do not contain commenting license text.
+String to be searched in Reverse Search doesn't have any limit on characters, for now.
+
+## Features
+
+- Find files that does NOT contain string 'X'
+
+## Upcoming features
+
+- Option for selecting case sensitive Reverse Search
+- Option for selectng MATCH WHOLE WORLD for Reverse Search
+
+## Usage guide 
+
+- ctrl + shift + p (for mac cmb + shift + p) to open command palette.
+- Enter string in command input that you wish to Reverse Search and hit Enter.
+- Output tab will open up with the list of files that don't include your string.
+
+## Known Issues
+
+No issue found for current version
+
+## Release Notes
+
+### 1.0.0
+
+functionality to search a list of files not containing specific string.
+
+
+-----------------------------------------------------------------------------------------------------------
+
---
+++ extension.js
@@ -0,0 +1,64 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "reverse-search" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.reverseSearch', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Enter a string in Command input!');
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
+		quickPick.placeholder = 'Enter a String';
+		quickPick.onDidAccept(() => {
+			const files = vscode.workspace.findFiles('**/*.{js,scss,md}', '**/node_modules/**');
+			files.then(response => {
+				vscode.window.showInformationMessage('processing...');
+				let reverseSearchOutPut = vscode.window.createOutputChannel("reverse-search");
+				reverseSearchOutPut.show();
+				let docArray = response.map((elm, i) => {
+					let doc = vscode.workspace.openTextDocument(vscode.Uri.file(elm.path))
+					doc.then(res => {
+						let docText = res.getText().replace(/[-[\]{}()*+?.\\^$|#\s]/g, '');
+						let searchString = quickPick.value.replace(/[-[\]{}()*+?.\\^$|#\s]/g, '');
+						if (docText.search(`${searchString}`) < 0) {
+							reverseSearchOutPut.appendLine(elm.path.substr(1));
+							return elm.path;
+						}
+						else {
+							return null;
+						}
+					});
+				});
+				vscode.window.showInformationMessage('Search completed! Check "OUTPUT" tab');
+			});
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
